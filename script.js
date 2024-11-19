c71d8b1859e7a4619004af2820a015f1c6c8a38a
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-19 20:35:01 -0300

    function finalized

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const table = document.querySelector('table');
 const button = document.querySelector('.button');
 
-button.addEventListener('click', () => {
-
+button.addEventListener('click', (e) => {
+    e.preventDefault();
     let csv = ''
 
     for(let i = 0; i < table.rows.length; i++){
@@ -15,9 +15,9 @@ button.addEventListener('click', () => {
     csv += rows.slice(0, -1) + '\n'
 }
   
-    button.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(linhas));
+    button.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv));
     button.setAttribute('download', 'tabela.csv');
   
- 
+ console.log(csv)
 })
 
