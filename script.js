39b4dd7f336d85efcb3ceefcbcc43304d8d65641
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-19 20:22:20 -0300

    refactor function

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ const table = document.querySelector('table');
 const button = document.querySelector('.button');
 
 button.addEventListener('click', () => {
-    const rows = table.querySelectorAll('tr');
-    let linhas = ''
-    rows.forEach(row => {
-        linhas += row.innerText + '\n';
-    })
+  
     button.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(linhas));
     button.setAttribute('download', 'tabela.csv');
   
